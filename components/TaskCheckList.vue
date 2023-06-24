35983755ed7f5ab4e282e6041514f35ba448b730
Dulan Hewage <dulanhewage2@gmail.com>
2023-06-25 04:04:24 +0530

    create new base component - ProgressBar

--- components/TaskCheckList.vue
+++ components/TaskCheckList.vue
@@ -5,4 +5,11 @@
   >
     <BaseButton type="secondary" size="sm">Delete</BaseButton>
   </BaseTaskSubTitle>
+  <BaseProgressBar :progress="10" />
+  <div class="mt-2">
+    <div class="flex items-center">
+      <Icon name="mdi:checkbox-blank-circle-outline" size="18px" />
+      <div class="ml-2">Add an item</div>
+    </div>
+  </div>
 </template>
---
+++ components/base/ProgressBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-2 flex items-center">
+    <span class="mr-2 text-sm">{{ props.progress }}%</span>
+    <div class="relative w-full">
+      <div class="bg-slate-300 w-full h-1 rounded"></div>
+      <div
+        class="bg-teal-600 h-1 rounded absolute top-0 left-0"
+        :style="{ width: `${props.progress}%` }"
+      ></div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+// define progress prop with valdation
+const props = defineProps({
+  progress: {
+    type: Number,
+    required: true,
+    validator: (val: number) => val >= 0 && val <= 100,
+    default: 0,
+  },
+});
+</script>
